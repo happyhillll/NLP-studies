416023ec503c6a5836f2436dc2ea66b5cc6b74c5
김민지 : ) <happyhill99@gmail.com>
2023-07-12 23:31:33 +0900

    QA github research

--- Torch/QA/data.py
+++ Torch/QA/data.py
@@ -1,7 +1,9 @@
 # data.py
-
+# from transformers import AutoTokenizer
+from torch.utils.data import Dataset
 from datasets import load_dataset
 
-def get_qa_data():
-    dataset = load_dataset('klue', 'mrc')
-    return dataset['train'], dataset['validation']
+class ds:
+    def get_qa_data():
+        dataset = load_dataset('klue', 'mrc')
+        return dataset['train'], dataset['validation']
