891a500b88666cdb581e056b2b2946e4e5e7e605
김민지 <happyhill99@gmail.com>
2023-07-13 17:44:35 +0900

    Update QAdata.py

--- Torch/QA/QAdata.py
+++ Torch/QA/QAdata.py
@@ -8,16 +8,26 @@ print(dataset['train'][0])
 
 class ds:
     def __init__(self):
-        self.get_qa_data()
         self.x_train_q = []
         self.x_train_context = []
+        self.y_train = []
+        for i in range(len(dataset['train'])):
+            self.x_train_q.append(dataset['train'][i]['question'])
+            self.x_train_context.append(dataset['train'][i]['context'])
+            self.y_train.append(dataset['train'][i]['answers']['text'][0])
     
-    def get_qa_data():
-        dataset = load_dataset('klue', 'mrc')
-        return dataset['train'], dataset['validation']
+        self.x_val_q = []
+        self.x_val_context = []
+        self.y_val = []
+        for i in range(len(dataset['validation'])):
+            self.y_train_q.append(dataset['validation'][i]['question'])
+            self.y_train_context.append(dataset['validation'][i]['context'])
+            self.y_train.append(dataset['validation'][i]['answers']['text'][0])
     
     def get_train(self):
-        self.x_train_q.append(dataset['train'][0]['question'])
-        self.x_train_context.append(dataset['train'][0]['context'])
-        return self.x_train_q, self.x_train_context
+        return self.x_train_q, self.x_train_context, self.y_train
+    
+    def get_val(self):
+        return self.x_val_q, self.x_val_context, self.y_val
+    
     
