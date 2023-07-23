9789bc9b3e89a61b04183988146fe078d3223f9c
김민지 : ) <happyhill99@gmail.com>
2023-07-23 22:17:41 +0900

    roman-udru downloading data

---
+++ Torch/roman_urdu_hate_speech/data.py
@@ -0,0 +1,6 @@
+#data.py
+from torch.utils.data import dataset
+
+#집 : /Users/minjikim/GitHub/NLP-studies/Torch/roman_urdu_hate_speech/data/task_1_train.tsv
+
+with 
---
+++ Torch/roman_urdu_hate_speech/main.py
@@ -0,0 +1,13 @@
+'''
+Task 1: Coarse-grained Classification Labels
+0:	Abusive/Offensive
+1:	Normal
+
+
+Task 2: Fine-grained Classification Labels
+0:	Abusive/Offensive
+1:	Normal
+2:	Religious Hate
+3:	Sexism
+4:	Profane/Untargeted
+'''
